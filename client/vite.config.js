500c021f5d876a920f21566fe177ae8e2acf22ec
RewSuphakit <rewkisuphakit44@gmail.com>
2026-01-21 16:42:39 +0700

    feat: Initialize Vite configuration for React with a base path.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/RMUTI/",
-  plugins: [react()], server: {
-    allowedHosts: [
-      'd384c84ab32a.ngrok-free.app'
-    ]
-  }
+  plugins: [react()],
 })
