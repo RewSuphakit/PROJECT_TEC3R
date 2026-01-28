fccb4853d0aabcd51677cf0502198f648319c046
RewSuphakit <rewkisuphakit44@gmail.com>
2026-01-29 00:06:39 +0700

    fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -351,7 +351,7 @@ function Home() {
               <div className="col-span-full text-center text-gray-500">กำลังโหลดข้อมูล...</div>
             ) : currentItems.length > 0 ? (
               currentItems.map((item) => (
-                <div key={item.equipment_id} className="relative bg-white border rounded-lg shadow-md transform transition duration-500 hover:scale-105">
+                <div key={item.equipment_id} className="relative bg-white border rounded-lg shadow-md transform transition duration-500 hover:scale-105 flex flex-col h-full">
                   <div className="aspect-w-16 aspect-h-9 p-2">
                     <img
                       className="rounded-md min-w-[100px] min-h-[100px] object-cover object-center"
@@ -362,14 +362,14 @@ function Home() {
                       height="200"
                     />
                   </div>
-                  <div className="px-4 pb-3">
+                  <div className="px-4 pb-3 flex flex-col flex-grow">
                     <h5 className="text-base sm:text-lg tracking-tight hover:text-sky-700 text-gray-900">
                       {item.equipment_name}
                     </h5>
                     <p className="text-sm text-gray-600 break-all">จำนวนคงเหลือ {item.available_quantity}</p>
                     <p className="text-xs text-gray-500 mt-2">อัพเดทเมื่อ: {new Date(item.updated_at).toLocaleString()}</p>
                     <hr className="w-full max-w-[12rem] h-1 mx-auto my-4 bg-gray-100 border-0 rounded" />
-                    <div className="text-center pb-2">
+                    <div className="text-center pb-2 pb-2 mt-auto">
                       {item.available_quantity === 0 ? (
                         <button className="w-full sm:w-auto px-4 py-2 text-sm sm:text-base font-medium text-white rounded-md shadow-sm bg-red-300 cursor-not-allowed">
                           หมด
