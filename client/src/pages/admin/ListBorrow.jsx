5755f69ca3f0aa80f1fb82cc15f857d494378943
RewSuphakit <rewkisuphakit44@gmail.com>
2026-01-30 02:01:36 +0700

    edit ListBorrw

--- client/src/pages/admin/ListBorrow.jsx
+++ client/src/pages/admin/ListBorrow.jsx
@@ -172,7 +172,7 @@ function ListBorrow() {
                   </thead>
                   <tbody>
                     {currentGroupKeys.length ? (
-                      currentGroupKeys.map((groupId) => {
+                      currentGroupKeys.map((groupId, index) => {
                         const groupItems = groupedRecords[groupId];
                         if (groupItems.length === 1) {
                           const item = groupItems[0];
